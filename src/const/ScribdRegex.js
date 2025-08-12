ca358e84606f5cb7303404df1aeed8104a48a8e5
tattahs <thaiscbmello@gmail.com>
2025-08-12 19:40:31 -0300

    Add files via upload
    Change so you can use pt.scribd as well

--- src/const/ScribdRegex.js
+++ src/const/ScribdRegex.js
@@ -1,4 +1,4 @@
-const DOMAIN = /^https:\/\/www\.scribd\.com/
+const DOMAIN = /^https:\/\/([a-z]{2}\.|www\.)scribd\.com/
 const DOCUMENT = /^https:\/\/www\.scribd\.com\/(document|doc)\/([0-9]+)/
 const EMBED = /^https:\/\/www\.scribd\.com\/embeds\/([0-9]+)/
 
