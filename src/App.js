e1e7fef0cfe64faa86b424b5f261f3b9d81eeea3
tattahs <thaiscbmello@gmail.com>
2025-08-14 02:38:22 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import { scribdDownloader } from "./service/ScribdDownloader.js"
-import { slideshareDownloader } from "./service/SlideshareDownloader.js"
-import { everandDownloader } from "./service/EverandDownloader.js"
-import * as scribdRegex from "./const/ScribdRegex.js"
-import * as slideshareRegex from "./const/SlideshareRegex.js"
-import * as everandRegex from "./const/EverandRegex.js"
+import { scribdDownloader } from "./service/ScribdDownloader.js";
+import { slideshareDownloader } from "./service/SlideshareDownloader.js";
+import { everandDownloader } from "./service/EverandDownloader.js";
+import * as scribdRegex from "./const/ScribdRegex.js";
+import * as slideshareRegex from "./const/SlideshareRegex.js";
+import * as everandRegex from "./const/EverandRegex.js";
 
-class App {
-    constructor() {
-        if (!App.instance) {
-            App.instance = this
-        }
-        return App.instance
-    }
-
-    async execute(url, flag) {
-        if (url.match(scribdRegex.DOMAIN)) {
-            await scribdDownloader.execute(url, flag)
-        } else if (url.match(slideshareRegex.DOMAIN)) {
-            await slideshareDownloader.execute(url)
-        } else if (url.match(everandRegex.DOMAIN)) {
-            await everandDownloader.execute(url)
-        } else {
-            throw new Error(`Unsupported URL: ${url}`)
-        }
-    }
+// normalize helper (yours)
+function normalizeScribdUrl(raw) {
+  const u = new URL(raw);
+  if (/^[a-z]{2}$/i.test(u.hostname.split(".")[0]) || u.hostname.startsWith("m.")) {
+    u.hostname = "www.scribd.com";
+  }
+  if (u.pathname.startsWith("/doc/")) {
+    u.pathname = u.pathname.replace("/doc/", "/document/");
+  }
+  return u.toString();
 }
 
-export const app = new App()
+class App {
+  constructor() {
+    if (!App.instance) App.instance = this;
+    return App.instance;
+  }
+
+  async execute(url, flag, pagesSpec = "") {
+    console.log("APP.execute pagesSpec=", pagesSpec || "(none)");
+    url = normalizeScribdUrl(url);
+
+    if (url.match(scribdRegex.DOMAIN)) {
+      await scribdDownloader.execute(url, flag, pagesSpec);   // ← pass it
+    } else if (url.match(slideshareRegex.DOMAIN)) {
+      await slideshareDownloader.execute(url);
+    } else if (url.match(everandRegex.DOMAIN)) {
+      await everandDownloader.execute(url);
+    } else {
+      throw new Error(`Unsupported URL: ${url}`);
+    }
+  }
+}
+
+export const app = new App();
